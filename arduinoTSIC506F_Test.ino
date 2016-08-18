a50782ee878884ad548e213570be13867f88de80
Simon Wyss <wysi94@gmail.com>
2016-08-18 13:46:18 +0200

    license update

--- arduinoTSIC506F_Test.ino
+++ arduinoTSIC506F_Test.ino
@@ -1,3 +1,22 @@
+/*
+arduinoTSIC506F_Test - A test programm fo the TSIC506F temperature sensor
+Copyright (C) 2016-2016 Simon Wyss <wysi94@gmail.com>
+
+This file is part of arduinoTSIC506F_Test.
+
+arduinoTSIC506_Test is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+arduinoTSIC506F_Test is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with arduinoTSIC506F_Test.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "TSIC.h"	// TSIC temperature sensor library
 
